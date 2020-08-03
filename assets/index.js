3a71473dccb868254d713f34e0481acabcf561d3
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-08-03 16:50:53 -0500

    add plugins for mute and unmute

--- assets/index.js
+++ assets/index.js
@@ -1,15 +1,11 @@
+import {mediaPlayer}  from "./MediaPlayer.js"
+import {AutoPlay} from "./plugins/AutoPlay.js"
+
 const video = document.querySelector("video")
-const button = document.querySelector("button")
+const button1 = document.querySelector(".button1")
+const button2 = document.querySelector(".button2")
 
-function mediaPlayer(config) {
-    this.media = config.el
-}
+const player = new mediaPlayer({ el : video, plugins: [new AutoPlay()] })
 
-mediaPlayer.prototype.play = function () {
-    this.media.paused
-        ? this.media.play()
-        : this.media.pause()
-}
-
-const player = new mediaPlayer({ el : video })
-button.onclick = () => player.play()
+button1.onclick = () => player.play()
+button2.onclick = () => player.unmute()
