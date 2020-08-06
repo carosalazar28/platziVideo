68619b339054f4efcbd647e006d76f98d69ba9c7
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-08-05 20:26:09 -0500

    Add file for make a observer in player

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,15 @@
 import {mediaPlayer}  from "./MediaPlayer.js"
 import {AutoPlay} from "./plugins/AutoPlay.js"
+import {AutoPause} from "./plugins/AutoPause.js"
 
 const video = document.querySelector("video")
 const button1 = document.querySelector(".button1")
 const button2 = document.querySelector(".button2")
 
-const player = new mediaPlayer({ el : video, plugins: [new AutoPlay()] })
+const player = new mediaPlayer({ 
+    el : video, 
+    plugins: [new AutoPlay(), new AutoPause()], 
+})
 
 button1.onclick = () => player.play()
 button2.onclick = () => player.unmute()
