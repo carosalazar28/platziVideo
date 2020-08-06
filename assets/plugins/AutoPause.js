08ee2eb33a3f0b95539a9a6b39317aae6345d510
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-08-06 18:07:04 -0500

    add plugins with problems

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -1,32 +1,42 @@
 class AutoPause {
     constructor() {
         this.threshold = 0.25
-        this.handlerIntersection = this.handlerIntersection.bind(this)
+        this.handleIntersection = this.handleIntersection.bind(this)
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
     }
 
     run(player) {
         this.player = player
 
-        const observer = new IntersectionObserver(this.handlerIntersection, {
-          threshold: this.threshold  
+        const observer = new IntersectionObserver(this.handleIntersection, {
+          threshold: this.threshold,  
         })
 
         observer.observe(this.player.media)
+
+        document.addEventListener("visibilitychange", this.handleVisibilityChange)
     }
 
-    handlerIntersection(entries) {
+    handleIntersection(entries) {
         const entry = entries[0]
         
         const isVisible = entry.intersectionRatio >= this.threshold
 
-        if(isVisible) {
-            this.player.play()
-        } else {
-            this.player.pause()
-        }
-        
+        isVisible
+            ? this.player.play()
+            : this.player.pause()
+
         console.log(entry)
     }
+
+    handleVisibilityChange() {
+        const isVisible = document.visibilityState === "visible"
+        
+        isVisible
+            ? this.player.play()
+            : this.player.pause()
+    }
 }
+
 
 export {AutoPause}
--- assets/plugins/AutoPlay.js
+++ assets/plugins/AutoPlay.js
@@ -1,6 +1,9 @@
-function AutoPlay() {}
-AutoPlay.prototype.run = function(player) {
-    player.mute()
-    player.play()
+class AutoPlay {
+    constructor() { }
+    run(player) {
+        player.mute()
+        player.play()
+    }
 }
+
 export {AutoPlay}
