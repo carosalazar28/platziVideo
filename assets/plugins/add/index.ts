c968c8b1a6a1a355e8fc7c795fd2f249847c5b12
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-08-12 20:19:08 -0500

    made some fix for the ads plugin

--- assets/plugins/add/index.ts
+++ assets/plugins/add/index.ts
@@ -8,6 +8,7 @@ class AdsPlugin {
     private currentAd: Ad
     private adsContainer: HTMLElement
 
+
     constructor() {
         this.ads = Ads.getInstance()
         this.adsContainer = document.createElement('div')
@@ -16,7 +17,7 @@ class AdsPlugin {
 
     run(player: mediaPlayer) {
         this.player = player
-        this.player.container.appendChild(this.adsContainer)
+        this.player.container.append(this.adsContainer)
         this.media = this.player.media
         this.media.addEventListener('timeupdate', this.handleTimeUpdate)
     }
@@ -35,7 +36,22 @@ class AdsPlugin {
 
         const ad = this.ads.getAd()
         this.currentAd = ad
-        console.log(this.currentAd)
+        this.adsContainer.innerHTML = 
+        `<div class="ads">
+            <a class="ads__link" href="${this.currentAd.url}" target="_blank">
+                <img class="ads__img" src="${this.currentAd.imageUrl}" />
+            <div class="ads__info">
+                <h5 class="ads__title">${this.currentAd.title}</h5>
+                <p class="ads__body">${this.currentAd.body}</p>
+            </div>
+            </a>
+        </div>` 
+
+        setTimeout(() => {
+            this.currentAd = null
+            this.adsContainer.innerHTML = ''
+        }, 10000)
+        
     }
 }
 
